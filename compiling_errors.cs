689784851fe345d92185fb35464b75d41cf6cd05
celcius911 <aalperen@icloud.com>
2023-11-07 18:20:03 +0300

    compiling errors

---
+++ compiling_errors.cs
@@ -0,0 +1,11 @@
+namespace CompilingErrors
+{
+    public partial class CompilingErrorsClass
+    {
+        public void CompilingErrorsDers()
+        {
+            // Compliling error örneği
+            // System.Console.WriteLine("Deneme") // ";" eksik hata verir.
+        }
+    }
+}
